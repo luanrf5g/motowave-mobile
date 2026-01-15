7602f506f9e80636351e849ee69287742d89a354
luanrf5g <of.luan.rferreira@gmail.com>
2026-01-14 22:19:56 -0300

    update: tripServices.ts

--- src/services/tripServices.ts
+++ src/services/tripServices.ts
@@ -1,10 +1,10 @@
-import { LocationObjectCoords } from "expo-location";
 import { supabase } from "../lib/supabase";
 import { Alert } from "react-native";
 
+// tipagem do corpo das cidades
 export interface City {
   name: string,
-  state: string,
+  state: string;
   latitude: number,
   longitude: number
 }
@@ -14,24 +14,76 @@ export interface Route {
   longitude: number
 }
 
-interface TripData {
+// tipagem do body da requisição de criar uma trip
+export interface CreateTripDTO {
   title: string,
   distance: number,
   route: Route[],
   cities: City[]
 }
 
+// tipagem do response do getHistory
+export interface TripHistoryItem {
+  id: string,
+  created_at: string,
+  total_distance: number,
+  title: string,
+  start_lat: number | null,
+  start_lon: number | null
+}
+
 const timeoutPromise = (ms: number) => {
-  return new Promise((_, reject) => setTimeout(() => reject(new Error("Tempo limite excedido. Verifique sua Conexão.")), ms))
+  return new Promise((_, reject) => setTimeout(() => reject(new Error("Tempo limite excedido. Verifique sua conexao!")), ms))
 }
 
 export const TripServices = {
-  saveToCloud: async (data: TripData, userId: string): Promise<boolean> => {
+
+  // Função para buscar o histórico de viagens
+  getHistory: async (): Promise<TripHistoryItem[]> => {
     try {
-      const { data: { session }, error: sessionError } = await supabase.auth.getSession()
+      const { data, error } = await supabase
+        .from('trips')
+        .select('id, created_at, total_distance, title, start_lat, start_lon')
+        .order('created_at', { ascending: true })
+
+      if (error) throw error;
+      return data || [];
+    } catch (error: any) {
+      console.error("Erro ao buscar histórico: ", error.message)
+      throw error;
+    }
+  },
+
+  // Função para deletar uma viagem
+  deleteTrip: async (id: string): Promise<{ success: boolean, message?: string }> => {
+    try {
+      const { error, data } = await supabase
+        .from('trips')
+        .delete()
+        .eq('id', id)
+        .select()
+
+      if (error) throw error;
+
+      if (!data || data.length === 0) {
+        return {
+          success: false,
+          message: "Registro não encontrado ou sem permissão."
+        }
+      }
+
+      return { success: true }
+    } catch (error: any) {
+      return { success: false, message: error.message }
+    }
+  },
+
+  saveToCloud: async (data: CreateTripDTO, userId: string): Promise<boolean> => {
+    try {
+      const { data: session, error: sessionError } = await supabase.auth.getSession();
 
       if (!session || sessionError) {
-        const { data: refreshData, error: refreshError } = await supabase.auth.refreshSession()
+        const { data: refreshData, error: refreshError } = await supabase.auth.refreshSession();
         if (refreshError || !refreshData.session) {
           throw new Error("Erro na autenticação do usuário.")
         }
@@ -40,6 +92,8 @@ export const TripServices = {
       const firstPoint = data.route.length > 0 ? data.route[0] : null;
 
       const uploadProcess = async () => {
+
+        // Inserir Viagem
         const { data: tripData, error: tripError } = await supabase
           .from('trips')
           .insert({
@@ -55,10 +109,13 @@ export const TripServices = {
         if (tripError) throw new Error(`Erro ao salvar viagem: ${tripError.message}`)
         const tripId = tripData.id
 
+
+        // Transformar o JSON no modelo text WKT para geometry
         const routeWKT = data.route
           .map(p => `${p.longitude} ${p.latitude}`)
           .join(',')
 
+        // Inserir a rota
         const { error: routeError } = await supabase
           .from('trip_routes')
           .insert({
@@ -66,7 +123,7 @@ export const TripServices = {
             path: `LINESTRING(${routeWKT})`
           })
 
-        if (routeError) throw new Error(`Erro na rota: ${routeError.message}`)
+        if (routeError) throw new Error(`Erro na rota: ${routeError.message}`);
 
         if (data.cities.length > 0) {
           const citiesToInsert = data.cities.map(c => ({
@@ -81,27 +138,25 @@ export const TripServices = {
             .from('visited_cities')
             .insert(citiesToInsert)
 
-          if (citiesError) console.warn(`Aviso: Erro ao salvar cidades: ${citiesError.message}`)
+          if (citiesError) console.warn(`Aviso: Erro ao salvar as cidades: ${citiesError.message}`)
         }
 
-        return true;
-      }
+        return true
+      };
 
       await Promise.race([uploadProcess(), timeoutPromise(15000)])
       return true;
     } catch (error: any) {
       console.log("TripService Error: ", error)
+      let msg = error.message;
 
-      let msg = "Falha ao salvar.";
-      if (error.message.includes("row-level security")) {
+      if (error.message.includes("row-level secutiry")) {
         msg = "Sessão expirada. Tente sair e entrar no app novamente."
-      } else if (error.message.includes("Tempo Limite")) {
-        msg = "Internet instável. Verifique sua conexão e tente novamente."
-      } else {
-        msg = error.message;
+      } else if (error.message.includes("Tempo limite")) {
+        msg = "Internet instável. Verifique sua conexao."
       }
 
-      Alert.alert("Erro no envio", msg)
+      Alert.alert("Erro no envio", msg);
       return false;
     }
   }
